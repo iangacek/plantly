f6bc219d39089b51bef5ef9cbe76ad05602baf6b
Ian Gacek <gacek004@umn.edu>
2019-09-14 13:10:38 -0500

    server might be working

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
+var mongoose = require("mongoose");
 const app = express();
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -11,12 +13,16 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/plantly";
+
+mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true });
+
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {
