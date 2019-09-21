09fd3b6aec9bbeccdf0611c61117c902f511ab76
Vincent Romine <vinjrom@live.com>
2019-09-21 11:22:12 -0500

    Remove unneccessary packages and Trefle Ajax calls

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require("path");
 var mongoose = require("mongoose");
 const app = express();
 const axios = require("axios");
-const fs = require("fs"); // Require File System data in .JSON
 const db = require("./models"); // Requires plant schema in models folder
 
 // Set server-port to 3001
@@ -26,27 +25,6 @@ mongoose.connect(MONGODB_URI, {
 });
 
 // Define API routes here
-// app.use(routes);
-
-// Treffle API plant search request
-// app.get("/API-search/:plantSearch", (req, res) => {
-//   console.log('endpoint hit');
-//   var tolken = "token=c1crZVFidEhCZzhoOTVnUWVyNFNZUT09";
-//   var endPoint = "https://trefle.io/api/plants?"+ tolken + "&q=" + req.params.plantSearch;
-//   axios.get(endPoint)
-//   .then(data => {res.json(data.data)})
-//     .catch(err => res.json(err));
-// });
-
-// Treffle API ID search
-// app.get("/ID-search/:id", (req, res) => {
-//   var tolken = "token=c1crZVFidEhCZzhoOTVnUWVyNFNZUT09";
-//   console.log(req.params.id)
-//   var endPoint = "https://trefle.io/api/plants/" + req.params.id + "?" + tolken;
-//   axios.get(endPoint)
-//   .then(data => {res.json(data.data)})
-//     .catch(err => res.json(err));
-// });
 
 // Plantly explore api route
 app.get("/plantly-explore", (req, res) => {
@@ -55,8 +33,10 @@ app.get("/plantly-explore", (req, res) => {
 
 // Plantly database API route
 app.get("/plantly-search/:plantName", (req, res) => {
-  console.log(req.params.plantName)
-  db.plant.find({ commonName: { "$regex": req.params.plantName, "$options": "i" } }).then(plants => res.json(plants));
+  console.log(req.params.plantName);
+  db.plant
+    .find({ commonName: { $regex: req.params.plantName, $options: "i" } })
+    .then(plants => res.json(plants));
 });
 
 // Default route to index.html
