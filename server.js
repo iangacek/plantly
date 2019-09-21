ceda6bda56fec960a64cb5e520c0c26f93344c01
Vincent Romine <vinjrom@live.com>
2019-09-21 13:34:56 -0500

    Gonna try reverting Haydn's changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 var mongoose = require("mongoose");
 const app = express();
 const axios = require("axios");
+const fs = require("fs"); // Require File System data in .JSON
 const db = require("./models"); // Requires plant schema in models folder
 
 // Set server-port to 3001
@@ -11,35 +12,51 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // References MongoDB database
-var MONGODB_URI = 'mongodb+srv://admin:admin@cluster0-fyxsd.mongodb.net/test?retryWrites=true&w=majority';
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/plantly";
 
 mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
-}).then( () => console.log('mongoose connection success'))
-.catch(err => console.log('mongoose connect err', err));
+});
 
 // Define API routes here
+// app.use(routes);
 
-// Plantly API explore route
+// Treffle API plant search request
+// app.get("/API-search/:plantSearch", (req, res) => {
+//   console.log('endpoint hit');
+//   var tolken = "token=c1crZVFidEhCZzhoOTVnUWVyNFNZUT09";
+//   var endPoint = "https://trefle.io/api/plants?"+ tolken + "&q=" + req.params.plantSearch;
+//   axios.get(endPoint)
+//   .then(data => {res.json(data.data)})
+//     .catch(err => res.json(err));
+// });
+
+// Treffle API ID search
+// app.get("/ID-search/:id", (req, res) => {
+//   var tolken = "token=c1crZVFidEhCZzhoOTVnUWVyNFNZUT09";
+//   console.log(req.params.id)
+//   var endPoint = "https://trefle.io/api/plants/" + req.params.id + "?" + tolken;
+//   axios.get(endPoint)
+//   .then(data => {res.json(data.data)})
+//     .catch(err => res.json(err));
+// });
+
+// Plantly exploure route
 app.get("/plantly-explore", (req, res) => {
-  console.log('/plantly-explore/ hit');
   db.plant.find().then(plants => res.json(plants));
 });
 
-// Plantly API search route
+// Plantly search route
 app.get("/plantly-search/:plantName", (req, res) => {
-  console.log(req.params.plantName);
-  db.plant
-    .find({ commonName: { $regex: req.params.plantName, $options: "i" } })
-    .then(plants => res.json(plants));
+  console.log(req.params.plantName)
+  db.plant.find({ commonName: { "$regex": req.params.plantName, "$options": "i" } }).then(plants => res.json(plants));
 });
 
 // Default route to index.html
@@ -53,5 +70,5 @@ app.listen(PORT, () => {
 });
 
 // Creates initial plant-database from plantlyAPI.json
-// var plants = require("./plantlyAPI.json")
-// db.plant.create(plants).then(plant => console.log(plant));
+var plantdb = require("./plantlyAPI.json")
+db.plant.create(plantdb).then(plant => console.log(plant));
