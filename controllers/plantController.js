0ee5b89307cc7abe01adfcb21b0c7c8b0e5b03a4
Ian Gacek <gacek004@umn.edu>
2019-09-21 11:34:00 -0500

    Update plantController.js

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -4,28 +4,28 @@ const db = require("../models");
 module.exports = {
   findAll: function(req, res) {
     db.plantdb.find(req.query)
-      .then(plantdb => res.json(plantdb))
+      .then(dbPlant => res.json(dbPlant))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
     db.plantdb.findById(req.params.id)
-      .then(plantdb => res.json(plantdb))
+      .then(dbPlant => res.json(dbPlant))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     db.plantdb.create(req.body)
-      .then(plantdb => res.json(plantdb))
+      .then(dbPlant => res.json(dbPlant))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     db.plantdb.findOneAndUpdate({ id: req.params.id }, req.body)
-      .then(plantdb => res.json(plantdb))
+      .then(dbPlant => res.json(dbPlant))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
     db.plantdb.findById(req.params.id)
-      .then(plantdb => plantdb.remove())
-      .then(plantdb => res.json(plantdb))
+      .then(dbPlant => dbPlant.remove())
+      .then(dbPlant => res.json(dbPlant))
       .catch(err => res.status(422).json(err));
   }
 };
