18f5428e99c8732681dd3ad68fcf931f787e668f
Ian Gacek <gacek004@umn.edu>
2019-09-23 21:58:17 -0500

    Used Vince's server file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 var mongoose = require("mongoose");
 const app = express();
+const axios = require("axios");
+const fs = require("fs"); // Require File System data in .JSON
 const db = require("./models"); // Requires plant schema in models folder
-
 // Set server-port to 3001
 const PORT = process.env.PORT || 3001;
-
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,18 +13,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 // References MongoDB database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/plantly";
-
 mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
-
+mongoose.set("useCreateIndex", true);
 // Define API routes here
-// app.use(routes);
-
 // Treffle API plant search request
 // app.get("/API-search/:plantSearch", (req, res) => {
 //   console.log('endpoint hit');
@@ -34,7 +30,6 @@ mongoose.connect(MONGODB_URI, {
 //   .then(data => {res.json(data.data)})
 //     .catch(err => res.json(err));
 // });
-
 // Treffle API ID search
 // app.get("/ID-search/:id", (req, res) => {
 //   var tolken = "token=c1crZVFidEhCZzhoOTVnUWVyNFNZUT09";
@@ -44,28 +39,33 @@ mongoose.connect(MONGODB_URI, {
 //   .then(data => {res.json(data.data)})
 //     .catch(err => res.json(err));
 // });
-
 // Plantly explore api route
 app.get("/plantly-explore", (req, res) => {
-  db.plant.find().then(plants => res.json(plants));
+  console.log("Entire list of plants should populate here");
+  try {
+    db.plant.find({}).then(plants => {
+      res.json(plants);
+      console.log(plants);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
-
 // Plantly database API route
 app.get("/plantly-search/:plantName", (req, res) => {
-  console.log(req.params.plantName)
-  db.plant.find({ commonName: { "$regex": req.params.plantName, "$options": "i" } }).then(plants => res.json(plants));
+  console.log(req.params.plantName);
+  db.plant
+    .find({ commonName: { $regex: req.params.plantName, $options: "i" } })
+    .then(plants => res.json(plants));
 });
-
 // Default route to index.html
 app.get("*", (req, res) => {
   // res.sendFile(path.join(__dirname, "./client/build/index.html"));
   res.json("Hello!");
 });
-
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
-
-// Creates initial plant-database from plantlyAPI.json
-// var plants = require("./plantlyAPI.json")
-// db.plant.create(plants).then(plant => console.log(plant));
+// Creates initial plant-database
+var plantdb = require("./plantlyAPI.json")
+db.plant.create(plantdb).then(plant => console.log(plant));
