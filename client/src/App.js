527ac13b3ebfbe91445624fbf6967ceb9f210f59
Michaela B <m.a.banczak@gmail.com>
2019-09-26 21:23:31 -0500

    starting router

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,82 @@
 import React, { Component } from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import ReactDOM from "react-dom";
-import Navbar from "./components/Navbar/navbar"; 
-import Explore from "./components/Explore/explore"; 
+import axios from 'axios'
+// import ReactDOM from "react-dom";
+import Navbar from "./components/Navbar/navbar";
+import Explore from "./components/Explore/explore";
 import Search from "./components/Search/search";
 import Home from "./components/Home/home";
+import Signup from './components/sign-up/sign-up';
+import LoginForm from './components/login-form/login-form';
 import myGarden from "./components/MyGarden/myGarden";
-
 class App extends Component {
-
-  render() {
-    return (
-        <Router>
-            <Navbar />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/explore" component={Explore} />
-              <Route exact path="/search" component={Search} />
-              <Route exact path="/myGarden" component={myGarden} />
-            </Switch>
-        </Router>
-    );
-  }
+ constructor() {
+   super()
+   this.state = {
+     loggedIn: false,
+     username: null
+   }
+   this.getUser = this.getUser.bind(this)
+   this.componentDidMount = this.componentDidMount.bind(this)
+   this.updateUser = this.updateUser.bind(this)
+ }
+ componentDidMount() {
+   this.getUser()
+ }
+ updateUser (userObject) {
+   this.setState(userObject)
+ }
+ getUser() {
+   axios.get('/user/').then(response => {
+     console.log('Get user response: ')
+     console.log(response.data)
+     if (response.data.user) {
+       console.log('Get User: There is a user saved in the server session: ')
+       this.setState({
+         loggedIn: true,
+         username: response.data.user.username
+       })
+     } else {
+       console.log('Get user: no user');
+       this.setState({
+         loggedIn: false,
+         username: null
+       })
+     }
+   })
+ };
+ render() {
+   return (
+     <div>
+       <Router>
+       <Navbar updateUser={this.updateUser} loggedIn={this.state.loggedIn} />
+       {!this.state.loggedIn ?
+       // if user not logged in,  force login
+       <Switch>
+         <Route
+         path="/signup"
+         render={() =>
+           <Signup/>}
+         />
+         <Route  //no path on this route means it is a catch-all
+         render={() =>
+             <LoginForm
+             updateUser={this.updateUser}
+             />
+           }
+           />
+       </Switch>
+       :
+         <Switch>
+           <Route exact path="/" component={Home} />
+           <Route exact path="/explore" component={Explore} />
+           <Route exact path="/search" component={Search} />
+           <Route exact path="/myGarden" component={myGarden} />
+         </Switch>
+       }
+     </Router>
+     </div>
+   );
 }
-
+}
 export default App;
